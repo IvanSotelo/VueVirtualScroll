5f97365582bdf1e8429fef7ebdf3159a0c948b14
Ivan Sotelo <isotelo@controlla.com.mx>
2019-05-23 20:29:28 -0600

    feat(commitslint): add package

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import VirtualScroll from './components/VirtualScroll'
 export default {
   install (Vue) {
     Vue.component('virtual-scroll', VirtualScroll)
-    Vue.mixin({
-      // Add component lifecycle hooks or properties.
-      created () {
-        console.log('%c Created By Ivan Sotelo %c 1.0.0 ', 'background: #35495e; color: #fff', 'background: #60b883; color: #fff')
-      }
-    })
   }
 }
 
